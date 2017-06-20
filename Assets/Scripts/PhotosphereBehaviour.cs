6fe36a1ef0b5225022c68cb78a123295326d904a
KHalkjaer <halkjaer.inc@gmail.com>
2017-06-20 14:20:17 +0200

    20.06 - Converting UI to World Space

--- Assets/Scripts/PhotosphereBehaviour.cs
+++ Assets/Scripts/PhotosphereBehaviour.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotosphereBehaviour : MonoBehaviour {
 	
 	[Header("Variables")]
 	public int sphereCount;
+	public bool legacyUI;
+	public SaveLoadUtility slu;
 
 	[Header("Paths")]
 	public string imagePath;
@@ -14,11 +17,13 @@ public class PhotosphereBehaviour : MonoBehaviour {
 	public string rightSphere;
 	public List<string> leftSphereTextures;
 	public List<string> rightSphereTextures;
+	public string savePath;
 
 	[Header("State Machine")]
 	public string state;
 
 	[Header("Objects")]
+	public GameObject UI;
 	public Transform sphere;
 	public Transform newestSphere;
 	[DontSaveMember]public GameObject quad;
@@ -26,8 +31,20 @@ public class PhotosphereBehaviour : MonoBehaviour {
 	[Header("Style Settings")]
 	[DontSaveMember]public GUIStyle style;
 	
+	// Initialization
 	void Start () {
-		// Nothing happening here at the moment 
+		UI = GameObject.FindWithTag("UI");
+		quad = GameObject.FindWithTag("Floorplan");
+	
+		if(slu == null) {
+			slu = GetComponent<SaveLoadUtility>();
+			if(slu == null) {
+				Debug.Log("[SaveLoadMenu] Start(): Warning! SaveLoadUtility not assigned!");
+			}
+		}
+
+		// Debug
+		Debug.Log(Application.dataPath);
 	}
 
 	// Switch floorplan material when the path is changed
@@ -40,12 +57,11 @@ public class PhotosphereBehaviour : MonoBehaviour {
 	}	
 	
 	// Update is called once per frame
-    void Update()
-    {
+    void Update() {
     	if (state == "placeSphere"){
 	        if (Input.GetMouseButtonDown(0)) {
 
-	        	// Raycast from camera to mouseposition
+	        	// Raycast from camera to mouse position
 	            RaycastHit hit;
 	            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -57,85 +73,153 @@ public class PhotosphereBehaviour : MonoBehaviour {
 					
         			// Increment Count and go back to Main State
         			sphereCount++;
-	                state = "main";
+	                ChangeState("main");
 	            }
 	        }
+    	}
+    } 
+
+    public void ChangeState(string newState){
+    	state = newState;
+
+    	// If 
+    	if(newState != "main"){
+    		UI.transform.GetChild(0).gameObject.SetActive(false);
+
+    		// Image Path
+    		if(newState == "setImagePath"){
+    			UI.transform.GetChild(1).gameObject.SetActive(true);
+    		}
+    		if(newState == "pathSet"){
+    			imagePath = UI.transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<InputField>().text;
+    			ChangeState("main");
+    		}
+
+    		// Floorplan
+    		if(newState == "loadFloor"){
+    			UI.transform.GetChild(2).gameObject.SetActive(true);
+    		}
+    		if(newState == "floorSet"){
+    			floorplanPath = UI.transform.GetChild(2).gameObject.transform.GetChild(0).GetComponent<InputField>().text;
+    			StartCoroutine(LoadFloorTexture());
+    			ChangeState("main");
+    		}
+
+    		// Define Sphere
+    		if(newState == "defineSphere"){
+    			UI.transform.GetChild(3).gameObject.SetActive(true);
+    		}
+
+    		// Place Sphere
+    		if(newState == "placeSphere"){
+    			// Disable all ui elements
+    			UI.transform.GetChild(0).gameObject.SetActive(false);
+	    		UI.transform.GetChild(1).gameObject.SetActive(false);
+	    		UI.transform.GetChild(2).gameObject.SetActive(false);
+	    		UI.transform.GetChild(3).gameObject.SetActive(false);
+	    		//
+
+    			leftSphereTextures.Add(UI.transform.GetChild(3).gameObject.transform.GetChild(0).GetComponent<InputField>().text);
+	        	rightSphereTextures.Add(UI.transform.GetChild(3).gameObject.transform.GetChild(1).GetComponent<InputField>().text);
+    		}
+
+    		if(newState == "saveGame"){
+    			slu.SaveGame(UI.transform.GetChild(4).gameObject.transform.GetChild(0).GetComponent<InputField>().text);
+    			Debug.Log(UI.transform.GetChild(4).gameObject.transform.GetChild(0).GetComponent<InputField>().text);
+    			ChangeState("Main");
+    		}    		
+
+    		if(newState == "loadGame"){
+    			//slu.SaveGame(UI.transform.GetChild(4).gameObject.transform.GetChild(0).GetComponent<InputField>().text);
+    			slu.LoadGame(UI.transform.GetChild(4).gameObject.transform.GetChild(0).GetComponent<InputField>().text);
+    			ChangeState("Main");
+    		}
+    	}
+
+    	// Activate the main menu elements
+    	else{
+    		UI.transform.GetChild(0).gameObject.SetActive(true);
+    		UI.transform.GetChild(1).gameObject.SetActive(false);
+    		UI.transform.GetChild(2).gameObject.SetActive(false);
+    		UI.transform.GetChild(3).gameObject.SetActive(false);
     	}
     }
 
     void OnGUI() {
-    	// Debug
+    	// Current State Debug
     	GUI.Label(new Rect(Screen.width-100, 0, 100, 20), state, style);
 
-    	// Main Screen
-    	if(state == "main"){
-    		// Main screen button
-    		if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-3.75f*Screen.height/9, Screen.width/7, Screen.height/9), "Set Image Path")){
-	        	state = "setImagePath";
-	        }
+    	if(legacyUI){
+	    	// Main Screen
+	    	if(state == "main"){
+	    		// Main screen button
+	    		if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-3.75f*Screen.height/9, Screen.width/7, Screen.height/9), "Set Image Path")){
+		        	state = "setImagePath";
+		        }
 
-	        // Main screen button
-	        if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-2.5f*Screen.height/9, Screen.width/7, Screen.height/9), "Load Floorplan")){
-	        	state = "loadFloor";
-	        }
+		        // Main screen button
+		        if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-2.5f*Screen.height/9, Screen.width/7, Screen.height/9), "Load Floorplan")){
+		        	state = "loadFloor";
+		        }
 
-	        // Main screen button
-	       	if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-1.25f*Screen.height/9, Screen.width/7, Screen.height/9), "Place Photosphere")){
-	        	state = "defineSphere";
-	        }
-    	}
+		        // Main screen button
+		       	if (GUI.Button(new Rect(Screen.width-1.1f*Screen.width/7, Screen.height-1.25f*Screen.height/9, Screen.width/7, Screen.height/9), "Place Photosphere")){
+		        	state = "defineSphere";
+		        }
+	    	}
 
-    	// Change Imagepath
-    	if(state == "setImagePath"){
+	    	// Change Imagepath
+	    	if(state == "setImagePath"){
 
-    		// Set variable based on text field
-    		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Image Path:", style);
-        	imagePath = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), imagePath, 50);
-	       	
-	       	if (GUI.Button(new Rect(Screen.width/2-Screen.width/14,  Screen.height/2+Screen.height/14, Screen.width/7, Screen.height/9), "Set")){
-	        	state = "main";
-	        } 
-    	}
+	    		// Set variable based on text field
+	    		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Image Path:", style);
+	        	imagePath = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), imagePath, 50);
+		       	
+		       	if (GUI.Button(new Rect(Screen.width/2-Screen.width/14,  Screen.height/2+Screen.height/14, Screen.width/7, Screen.height/9), "Set")){
+		        	state = "main";
+		        } 
+	    	}
 
-    	// Load Floorplan
-    	if(state == "loadFloor"){
+	    	// Load Floorplan
+	    	if(state == "loadFloor"){
 
-    		if(!quad){
-    			quad = GameObject.FindWithTag("Floorplan");
-    		}
+	    		if(!quad){
+	    			quad = GameObject.FindWithTag("Floorplan");
+	    		}
 
-    		// Set variable based on text field
-    		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Floorplan Image Name:", style);
-        	floorplanPath = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), floorplanPath, 50);
-	       	
-	       	if (GUI.Button(new Rect(Screen.width/2-Screen.width/14,  Screen.height/2+Screen.height/14, Screen.width/7, Screen.height/9), "Load")){
-	        	StartCoroutine(LoadFloorTexture());
-	        	state = "main";
-	        } 
-    	}
+	    		// Set variable based on text field
+	    		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Floorplan Image Name:", style);
+	        	floorplanPath = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), floorplanPath, 50);
+		       	
+		       	if (GUI.Button(new Rect(Screen.width/2-Screen.width/14,  Screen.height/2+Screen.height/14, Screen.width/7, Screen.height/9), "Load")){
+		        	StartCoroutine(LoadFloorTexture());
+		        	state = "main";
+		        } 
+	    	}
 
-    	// Settings for new photosphere
-    	if(state == "defineSphere"){
-    		// Left Eye
-    		// Set variable based on text field
-       		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-Screen.height/9-20, 100, 20), "Left Sphere Name:", style);
-        	leftSphere = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2-Screen.height/9, Screen.width/2, Screen.height/18), leftSphere, 50);
+	    	// Settings for new photosphere
+	    	if(state == "defineSphere"){
+	    		// Left Eye
+	    		// Set variable based on text field
+	       		GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-Screen.height/9-20, 100, 20), "Left Sphere Name:", style);
+	        	leftSphere = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2-Screen.height/9, Screen.width/2, Screen.height/18), leftSphere, 50);
 
-        	// Right Eye
-        	// Set variable based on text field
-        	GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Right Sphere Name:", style);
-        	rightSphere = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), rightSphere, 50);
+	        	// Right Eye
+	        	// Set variable based on text field
+	        	GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-20, 100, 20), "Right Sphere Name:", style);
+	        	rightSphere = GUI.TextField(new Rect(Screen.width/2-Screen.width/4, Screen.height/2, Screen.width/2, Screen.height/18), rightSphere, 50);
 
-        	if (GUI.Button(new Rect(Screen.width/2-Screen.width/7-Screen.width/21,  Screen.height/2+Screen.height/10, Screen.width/7, Screen.height/9), "Load")){
-        		leftSphereTextures.Add(leftSphere);
-        		rightSphereTextures.Add(rightSphere);
+	        	if (GUI.Button(new Rect(Screen.width/2-Screen.width/7-Screen.width/21,  Screen.height/2+Screen.height/10, Screen.width/7, Screen.height/9), "Load")){
+	        		leftSphereTextures.Add(leftSphere);
+	        		rightSphereTextures.Add(rightSphere);
 
-	        	state = "placeSphere";
-	        }
+		        	state = "placeSphere";
+		        }
 
-			if (GUI.Button(new Rect(Screen.width/2+Screen.width/21,  Screen.height/2+Screen.height/10, Screen.width/7, Screen.height/9), "Cancel")){
-	        	state = "main";
-	        }
+				if (GUI.Button(new Rect(Screen.width/2+Screen.width/21,  Screen.height/2+Screen.height/10, Screen.width/7, Screen.height/9), "Cancel")){
+		        	state = "main";
+		        }
+	    	}
     	}
     }
 }
